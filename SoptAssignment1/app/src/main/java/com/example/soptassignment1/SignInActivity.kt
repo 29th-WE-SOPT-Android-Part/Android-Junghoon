531e2446a11823b4c275a16658fefb9c9edaa669
jeonghoon park <shb03323@naver.com>
2021-11-11 14:43:20 +0900

    [ADD] level 1

--- SoptAssignment1/app/src/main/java/com/example/soptassignment1/SignInActivity.kt
+++ SoptAssignment1/app/src/main/java/com/example/soptassignment1/SignInActivity.kt
@@ -3,10 +3,14 @@ package com.example.soptassignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.soptassignment1.databinding.ActivitySigninBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySigninBinding
@@ -28,14 +32,7 @@ class SignInActivity : AppCompatActivity() {
 
         // 홈 뷰로 이동
         binding.btnLogin.setOnClickListener {
-            if(binding.etID.text.isNullOrBlank() || binding.etPassword.text.isNullOrBlank()) {
-                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                Toast.makeText(this, "박정훈님 환영합니다", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-            }
+            initNetwork()
         }
 
         // 회원가입 뷰로 이동
@@ -43,5 +40,34 @@ class SignInActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             getLoginText.launch(intent)
         }
+    }
+
+    private fun initNetwork() {
+        val requestLoginData = RequestLoginData(
+            id = binding.etID.text.toString(),
+            password = binding.etPassword.text.toString()
+        )
+
+        val call: Call<ResponseLoginData> = LoginServiceCreator.loginService.postLogin(requestLoginData)
+
+        call.enqueue(object : Callback<ResponseLoginData> {
+            override fun onResponse(
+                call: Call<ResponseLoginData>,
+                response: Response<ResponseLoginData>
+            ) {
+                if(response.isSuccessful) {
+                    val data = response.body()?.data
+
+                    Toast.makeText(this@SignInActivity, "${data?.email}님 반갑습니다!", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@SignInActivity, HomeActivity::class.java))
+                }
+                else
+                    Toast.makeText(this@SignInActivity, "로그인에 실패하셨습니다", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFailure(call: Call<ResponseLoginData>, t: Throwable) {
+                Log.e("NetworkText", "error:$t")
+            }
+        })
     }
 }
--- SoptAssignment1/app/src/main/java/com/example/soptassignment1/SignUpActivity.kt
+++ SoptAssignment1/app/src/main/java/com/example/soptassignment1/SignUpActivity.kt
@@ -3,8 +3,12 @@ package com.example.soptassignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.soptassignment1.databinding.ActivitySignUpBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -17,15 +21,37 @@ class SignUpActivity : AppCompatActivity() {
 
         // 로그인 뷰로 이동
         binding.btnSignup.setOnClickListener {
-            if(binding.etName.text.isNullOrBlank() || binding.etID.text.isNullOrBlank() || binding.etPassword.text.isNullOrBlank())
-                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
-            else {
-           setResult(RESULT_OK, Intent(this, SignInActivity::class.java).apply {
-                      putExtra("ID", binding.etID.text.toString())
-                      putExtra("password", binding.etPassword.text.toString())
-           })
-                finish()
-            }
+            initNetwork()
+            finish()
         }
+    }
+
+    private fun initNetwork() {
+        val requestSignupData = RequestSignupData(
+            id = binding.etID.text.toString(),
+            name = binding.etName.text.toString(),
+            password = binding.etPassword.text.toString()
+        )
+
+        val call: Call<ResponseSignupData> = LoginServiceCreator.signupService.postSignup(requestSignupData)
+
+        call.enqueue(object : Callback<ResponseSignupData> {
+            override fun onResponse(
+                call: Call<ResponseSignupData>,
+                response: Response<ResponseSignupData>
+            ) {
+                if(response.isSuccessful) {
+                    val data = response.body()?.data
+
+                    Toast.makeText(this@SignUpActivity, "${data?.email}님 회원가입이 완료되었습니다!", Toast.LENGTH_SHORT).show()
+                }
+                else
+                    Toast.makeText(this@SignUpActivity, "회원가입에 실패하셨습니다", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onFailure(call: Call<ResponseSignupData>, t: Throwable) {
+                Log.e("NetworkText", "error:$t")
+            }
+        })
     }
 }
